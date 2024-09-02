feca1eedd080f1a5cff847e5dad677b45bcc960b
Criscadinu <cris.g.cadinu@gmail.com>
2024-09-02 10:35:44 +0200

    Remove Weather and Counter and tested API connection

---
+++ AglixFrontend/Models/Agile.cs
@@ -0,0 +1,9 @@
+﻿namespace AglixFrontend.Models
+{
+    public class Agile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
