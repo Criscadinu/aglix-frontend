ddb0ed231186e55187b462f3f48e07b019299cf0
Criscadinu <cris.g.cadinu@gmail.com>
2024-09-17 19:01:55 +0200

    Refactored and separated all code and css from razor

---
+++ AglixFrontend/Pages/Guide.razor.cs
@@ -0,0 +1,26 @@
+﻿using AglixFrontend.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace AglixFrontend.Pages
+{
+    public partial class Guide : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private List<Agile> agileTopics;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                agileTopics = await Http.GetFromJsonAsync<List<Agile>>("https://localhost:7060/api/agile");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching data: {ex.Message}");
+            }
+        }
+    }
+}
