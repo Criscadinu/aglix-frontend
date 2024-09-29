d3c79a366a7783187c8143ea774b9b32bb6f79f1
Criscadinu <cris.g.cadinu@gmail.com>
2024-09-29 16:57:01 +0200

    added information to scrumoverview and added new page Team Topologies

---
+++ AglixFrontend/Pages/ScrumOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AglixFrontend.Pages
+{
+    public partial class ScrumOverview
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        private void GoBack()
+        {
+            Navigation.NavigateTo("/guide");
+        }
+    }
+}
